aa308c70a69c45bd7e66b2cd2227e171ed15c000
Joseph Morris <joe@xenotropic.net>
2023-01-13 14:26:34 -0800

    Update genwiki.sh
    using just the requirements.txt file for requirements

--- genwiki.sh
+++ genwiki.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python -m pip install rotary_embedding_torch librosa einops inflect
+cd /workspace/tortoise-tts
+python -m pip install -r requirements.txt
 python -m pip install pysbd html5lib
-cd /notebooks/tortoise-tts
-python -m php install .
+python -m pip install .
 #right now this is just hardcoded to load the wikipedia top 25; now planning to make it more general. also using paperspace filespace naming conventions
 python /workspace/tortoise-wiki-tts/scripts/wikitop25tts.py > /notebooks/inputs/wikitop25.txt
 python /workspace/tortoise-wiki-tts/read.py --voice train_lescault --preset fast --output_path=/workspace/outputs/wikitop25 --textfile=/workspace/inputs/wikitop25.txt  2>&1 | tee -a /workspace/logs/wiki25log.txt
