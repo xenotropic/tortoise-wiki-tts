42b00a1a01ed958ba73623d5ef58afc0eaf8f32e
Joseph Morris <joe@xenotropic.net>
2023-01-13 13:49:36 -0800

    Update genwiki.sh
    changing to forked directory name

--- genwiki.sh
+++ genwiki.sh
@@ -4,8 +4,8 @@
 #cd /notebooks/tortoise-tts
 #python3 setup.py install
 #right now this is just hardcoded to load the wikipedia top 25; now planning to make it more general. also using paperspace filespace naming conventions
-python3 /workspace/scripts/wikitop25tts.py > /notebooks/inputs/wikitop25.txt
-python3 /workspace/tortoise-tts/read.py --voice train_lescault --preset fast --output_path=/workspace/outputs/wikitop25 --text-file=/workspace/inputs/wikitop25.txt  2>&1 | tee -a /workspace/logs/wiki25log.txt
+python3 /workspace/tortoise-wiki-tts/scripts/wikitop25tts.py > /notebooks/inputs/wikitop25.txt
+python3 /workspace/tortoise-wiki-tts/read.py --voice train_lescault --preset fast --output_path=/workspace/outputs/wikitop25 --text-file=/workspace/inputs/wikitop25.txt  2>&1 | tee -a /workspace/logs/wiki25log.txt
 
 # Things I have tried but that don't currently seem necessary
 #/notebooks/tortoise-tts/tortoise_tts.py --batch-size 12 -v william -p fast -O /notebooks/jm/jm-outputs/wikitop25 < /notebooks/jm/jm-inputs/wikitop25.txt 2>&1 | tee -a /notebooks/jm/jm-logs/wiki25log.txt
