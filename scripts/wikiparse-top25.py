18d6453f08f4f41a68328006f2d924ec0f79fadc
Joseph Morris <joe@xenotropic.net>
2023-01-17 21:21:32 -0800

    added ability to handle multiple rows with same text without repeating it

--- scripts/wikiparse-top25.py
+++ scripts/wikiparse-top25.py
@@ -5,6 +5,7 @@ import pysbd
 import re
 
 wikiurl = "Wikipedia%3ATop_25_Report"
+stringtoread = ""
 
 def make_ordinal(n):
     n = int(n)
@@ -20,7 +21,7 @@ def make_ordinal(n):
 def ordinal_replace(matchobj):
     num = matchobj.group(0)[1:] #using string slicing to remove leading #
     num_index = int (num) - 1 # text numbers start from one, dataframe from zero
-    return " number " + num + ", " + df.iloc[num_index]["Article"] + ", "
+    return " number " + num + ", " + df.iloc[num_index]["Article"] 
 
 # makes $1.2 billion into 1.2 billion dollars which TTS handles more gracefully
 
@@ -34,20 +35,21 @@ json_url = urlopen("https://en.wikipedia.org/w/api.php?action=parse&format=json&
 data = json.loads(json_url.read())
 title= data["parse"]["sections"][0]["line"]
 stringtoread = ("Here are the " + title + "." )
+
 #this gets the wikitables on the page, of which there is only one
 dfl = pandas.read_html("https://en.wikipedia.org/wiki/" + wikiurl , attrs={"class": "wikitable"}, flavor='html5lib');
 # that returns a list of dataframes, we want the first and only dataframe, which is the only wikitable on the page
 df = dfl[0]
-print (df)
-
 
 for row_index, row in df.iterrows():
+    stringtoread += "The " + make_ordinal (row["Rank"]) + " most popular article this week was " + row["Article"] + ". "
     notes =  row["Notes/about"]
-    notes = re.sub('#[0-9][0-9]?', ordinal_replace, notes)
-    notes = re.sub('\$[0-9.]* ?[bmtz]illion', money_replace, notes) 
-    to_append = ("The " + make_ordinal (row["Rank"]) + " most popular article this week was " + row["Article"] + ". " + notes + " " )
-    
-    stringtoread += to_append
+    if not pandas.isna(notes):
+        if ( row_index + 1 < len (df) ):
+            if (notes == df.iloc[row_index+1]["Notes/about"]): continue # this handles multiple entries with the same notes
+        notes = re.sub('#[0-9][0-9]?', ordinal_replace, notes)
+        notes = re.sub('\$[0-9.]* ?[bmtz]illion', money_replace, notes) 
+        stringtoread +=  notes + " "
 
 seg = pysbd.Segmenter(language="en", clean=False)
 sentences = seg.segment(stringtoread)
