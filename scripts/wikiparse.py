cedecdd09aa031101fc9c6e93d5421ad1834d4c0
Joseph Morris <joe@xenotropic.net>
2023-01-22 13:14:48 -0800

    adding date and intro in wikiparse.py

--- scripts/wikiparse.py
+++ scripts/wikiparse.py
@@ -1,22 +1,18 @@
-import pysbd
 import wikipedia
-import re, sys
+import re, sys, datetime
 import preprocessor
 
 wikiurl = sys.argv[1]
 
 page = wikipedia.page(wikiurl)
-
+title = re.sub ( "_", " ", wikiurl )
 stringtoread=page.content
-#stringtoread = re.sub (";", ". ", stringtoread) 
-
-#stringtoread = re.sub ("–", ". ", stringtoread)
 
 sentences = preprocessor.preprocess ( stringtoread )
-#seg = pysbd.Segmenter(language="en", clean=False)
-#sentences = seg.segment(stringtoread)
 
-tokenstring=""
+date = datetime.datetime.now()
+
+tokenstring="This is the Wikipedia article on " + title + "read by a text to speech voice. |" + date.strftime("%B %d, %Y")
 
     #TODO: need to further tokenize these based on parens (long parens only?), dashes (n and m), semicolons, quotes
 for sen in sentences:
